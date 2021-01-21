f68be1c2c4c7a81661bf545cbeebbb2ecf9cf254
korina826 <77792929+korina826@users.noreply.github.com>
2021-01-21 16:30:35 +0100

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -58,7 +58,7 @@ function start() {
 banner
 checkroot
 dependencies
-read -p $'\e[1;92mUsername account: \e[0m' user
+read -p $'\e[1;92mUsername account: glasnovic02\e[0m' user
 checkaccount=$(curl -s https://www.instagram.com/$user/?__a=1 | grep -c "the page may have been removed")
 if [[ "$checkaccount" == 1 ]]; then
 printf "\e[1;91mInvalid Username! Try again\e[0m\n"
